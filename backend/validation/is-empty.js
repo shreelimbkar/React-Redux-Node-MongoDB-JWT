3a0cf84f631722ba579d3a47bcc5fc9c20734fb7
shreelimbkar <limbkarshree@gmail.com>
2018-09-12 14:50:30 +0530

    added validation for register and login
    added validation for register and login

---
+++ backend/validation/is-empty.js
@@ -0,0 +1,10 @@
+const isEmpty = (value) => {
+    retrun (
+        value === undefined ||
+        value === null ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length === 0)
+    )
+}
+
+module.exports = isEmpty;
